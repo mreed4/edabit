e37a297f4805221b6fa10f697c5e18ad37109a54
Matthew <78713317+mreed4@users.noreply.github.com>
2022-02-08 22:54:46 -0800

    Refactor

--- integerDIgitsCount.js
+++ integerDIgitsCount.js
@@ -1,41 +1,16 @@
-function count(number) {
+// https://stackoverflow.com/a/28203456
 
-    // Get absolute value of number
-    number = Math.abs(number);
+const count = (n) => {
+  let result;
+  if (n === 0) {
+    result = 1;
+  } else {
+    result = Math.ceil(Math.log10(Math.abs(n)));
+  }
+  return result;
+};
 
-    // Convert it to a string
-    let str = String(number);
+let test = count(318);
+test = count(-314890);
 
-    // Return its length
-    return str.length;
-
-    /*
-    Alternate solution
-    */
-
-    // Initialize counter
-    let count = 0;
-
-    // Make given number its absolute value
-    number = Math.abs(number);
-
-    // Make the number a string
-    str = String(number);
-
-    // Split the string to an array
-    let arr = str.split('');
-
-    // Increase counter
-    arr = arr.forEach(n => {
-        Number(n);
-        if (n) {
-            count++;
-        }
-    });
-
-    // Magic
-    return count;
-}
-
-count(318);
-count(-314890);
+console.log(test);
