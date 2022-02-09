65ad7ee77522185dac158870e46056cc8e453439
Matthew <78713317+mreed4@users.noreply.github.com>
2022-02-08 22:24:05 -0800

    Refactored

--- countSyllables.js
+++ countSyllables.js
@@ -1,40 +1,7 @@
-function countSyllables(str) {
+const countSyllables = (str) => {
+  return [...str.toLowerCase()].filter((n) => n.match(/[aeiou]/g)).length;
+};
 
-    /* 
-    This solution would not work for 'words'
-    longer than two characters
+let test = countSyllables("Hehehehehehe");
 
-    This solution may be cheesing the intent of
-    the exercise a bit
-    */
-
-    // Initialize counter
-    let total = 0;
-
-    // Split given string into array
-    let arr = str.split('');
-
-    // Increase counter by one for each consonant
-    arr.forEach(n => {
-
-        // First convert each letter to lowercase
-        n.toLowerCase();
-
-        // Then check for consonants
-        if (n !== `a` ||
-            n !== `e` ||
-            n !== `i` ||
-            n !== `o` ||
-            n !== `u`) {
-
-            // Increase total by 1
-            total++
-        }
-    });
-
-    // Magic
-    return total / 2;
-
-}
-
-countSyllables("Hehehehehehe");
+console.log(test);
